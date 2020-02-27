f0a47bc183bfdc75a9b97cbcc3b0047277af39f4
rcdmoreno <rcdmoreno@gmail.com>
2020-02-27 02:08:39 -0300

    teste mongo cadastro

---
+++ Cadastro_MongoDB/Cadastro.py
@@ -0,0 +1,61 @@
+import pymongo
+
+client = pymongo.MongoClient()
+db = client.Cadastro
+
+def create_user(name, email, password):
+    db.users.insert({
+        'name': name,
+        'email': email,
+        'password': password
+    })
+
+def find_user_by_email(email):
+    return db.users.find({
+        'email': email
+    })
+
+def Listar_user():
+    for user in db.users.find({},{ '_id': 0, 'name': 1, 'email': 1}):
+        print(user)
+
+def delete_user_by_name(name):
+    return db.users.remove({
+        'name': name
+    })
+    
+print('1 - Cadastrar novo usuário;')
+print('2 - Consultar usuário por e-mail;')
+print('3 - Listar usuário Cadastrados;')
+print('4 - Remover Usuario pelo nome;')
+print('0 - Encerrar o Programa.\n')
+
+opcao = int(input('Digite a opção desejada: '))
+
+if opcao == 0:
+    print('Agradeçemos sua preferência.\n\n')
+
+elif opcao == 1:
+    name = input('Digite seu nome: ')
+    email = input('Digite seu email: ')
+    password = input('Digite sua senha: ')
+    create_user(name, email, password)
+
+elif opcao == 2:
+    email = input('Digite seu email: ')
+    print(find_user_by_email(email))
+
+elif opcao == 3:
+    user = Listar_user()
+
+elif opcao == 4:
+    name = input('Digite o nome do usuario a ser deletado: ')
+    print(delete_user_by_name(name))
+
+
+
+
+
+
+
+
--- aula_1/ex_2.py
+++ aula_1/ex_2.py
@@ -7,10 +7,10 @@
 
 nome = input("Qual seu nome: ? ")
 idade = input("Qual sua idade: ? ")
-email = input("Qual seu e-mail: ? ")
+#email = input("Qual seu e-mail: ? ")
 
 if int(idade) >= 18:
-    print(nome, idade, email)
+    print(nome, idade)#, email)
     print("Seja bem vindo ao Sistema")
 else:
     print("Acesso Bloqueado")
